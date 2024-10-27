d62e903d8d1ccb2f5623658d9c964d93da0ced1d
vladar21 <vladyslav.rastvorov@mycit.ie>
2024-10-27 15:22:02 +0000

    fix functions order in main

--- main.py
+++ main.py
@@ -2,28 +2,27 @@ import threading
 from recipe_manager import RecipeManager
 from recipe_item import RecipeFactory
 
+
+def create_cake_recipe():
+    print("Starting thread for Layer Cake")
+    frosting = factory.create_recipe('Dessert', 'Frosting', [('sugar', '1 cup'), ('butter', '1/2 cup'), ('vanilla', '1 tsp')], 'Mix all ingredients until smooth.')
+    cake_base = factory.create_recipe('Dessert', 'Cake Base', [('flour', '2 cups'), ('sugar', '1 cup'), ('eggs', '3')], 'Mix ingredients and bake at 350°F for 30 minutes.')
+    cake = factory.create_recipe('Dessert', 'Layer Cake', [('milk', '1 cup'), ('baking powder', '1 tbsp')], 'Prepare the layers and frost each layer.', subrecipes=[frosting, cake_base])
+
+    recipe_manager.add_recipe(cake)
+    print("Finished thread for Layer Cake")
+
+
+def create_lemonade_recipe():
+    print("Starting thread for Lemonade")
+    lemonade = factory.create_recipe('Beverage', 'Lemonade', [('water', '2 cups'), ('lemon', '1 whole'), ('sugar', '2 tbsp')], 'Mix ingredients and serve cold.')
+    recipe_manager.add_recipe(lemonade)
+    print("Finished thread for Lemonade")
+
+
 if __name__ == "__main__":
     recipe_manager = RecipeManager()
     factory = RecipeFactory()
-
-    # Define the target function for thread1 and thread2
-    def create_cake_recipe():
-        print("Starting thread for Layer Cake")
-        frosting = factory.create_recipe('Dessert', 'Frosting', [('sugar', '1 cup'), ('butter', '1/2 cup'), ('vanilla', '1 tsp')],
-                                         'Mix all ingredients until smooth.')
-        cake_base = factory.create_recipe('Dessert', 'Cake Base', [('flour', '2 cups'), ('sugar', '1 cup'), ('eggs', '3')],
-                                          'Mix ingredients and bake at 350°F for 30 minutes.')
-        cake = factory.create_recipe('Dessert', 'Layer Cake', [('milk', '1 cup'), ('baking powder', '1 tbsp')],
-                                     'Prepare the layers and frost each layer.', subrecipes=[frosting, cake_base])
-        recipe_manager.add_recipe(cake)
-        print("Finished thread for Layer Cake")
-
-    def create_lemonade_recipe():
-        print("Starting thread for Lemonade")
-        lemonade = factory.create_recipe('Beverage', 'Lemonade', [('water', '2 cups'), ('lemon', '1 whole'), ('sugar', '2 tbsp')],
-                                         'Mix ingredients and serve cold.')
-        recipe_manager.add_recipe(lemonade)
-        print("Finished thread for Lemonade")
 
     # Create threads for each recipe
     thread1 = threading.Thread(target=create_cake_recipe)
